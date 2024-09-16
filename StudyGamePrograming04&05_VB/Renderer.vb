001d629141dcba18b5cd890fd237f2fc43cfcea3
tanada@sandagakuen.ed.jp <tanada@sandagakuen.ed.jp>
2024-09-16 23:32:40 +0900

    update

---
+++ StudyGamePrograming04&05_VB/Renderer.vb
@@ -0,0 +1,230 @@
+﻿Imports System.IO.MemoryMappedFiles
+Imports System.Net.Mime.MediaTypeNames
+Imports OpenTK
+Imports OpenTK.Graphics
+Imports OpenTK.Graphics.OpenGL
+Imports OpenTK.Mathematics
+Imports OpenTK.Windowing.Common
+
+Public Class Renderer
+    Implements IDisposable      '明示的にクラスを開放するために必要
+
+    'public
+    Sub New(ByRef game As Game)
+        mGame = game
+    End Sub
+    Protected disposed = False     '開放処理が実施済みかのフラグ
+    Public Overloads Sub Dispose() Implements IDisposable.Dispose
+        Dispose(True)
+    End Sub
+    Protected Overridable Overloads Sub Dispose(ByVal disposing As Boolean)
+        If Not Me.disposed Then
+            If disposing Then
+                '*** アンマネージリソースの開放
+            End If
+            '*** マネージドリソースの開放
+        End If
+        disposed = True
+    End Sub
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+        Dispose(False)
+    End Sub
+    Public Function Initialize(ByVal screenWidth As Integer, ByVal screenHeight As Integer) As Boolean
+        mScreenWidth = screenWidth
+        mScreenHeight = screenHeight
+        ' 画面初期化
+        GL.Viewport(0, 0, screenWidth, screenHeight)
+
+        ' 頂点情報オブジェクトの生成
+        CreateVertexInfo()
+
+        ' シェーダーの生成
+        If (LoadShaders() <> True) Then
+            Console.WriteLine("シェーダーの読み込みに失敗しました。")
+            Return False
+        End If
+        Return True
+    End Function
+    Public Sub Shutdown()
+        UnloadData()
+        mVertexInfo.Dispose()
+        mShader.Unload()
+        mShader.Dispose()
+        Me.Dispose()
+    End Sub
+    Public Sub UnloadData()
+        mTextures.Clear()
+    End Sub
+    Public Sub Draw()
+        '背景色を指定して画面をクリア
+        GL.ClearColor(0.3, 0.3, 0.3, 1.0)
+        GL.Clear(ClearBufferMask.ColorBufferBit Or ClearBufferMask.DepthBufferBit)
+        GL.Enable(EnableCap.DepthTest)
+        'カラーバッファのアルファブレンディングを有効化
+        GL.Enable(EnableCap.Blend)
+        GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha)
+
+        ' シェーダーとバーテックス配列オブジェクトを有効化
+        mVertexInfo.SetActive()
+        mShader.SetActive()
+
+
+        mView = Matrix4.LookAt(Vector3.Zero, Vector3.UnitX, Vector3.UnitZ)
+        mProjection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.Pi * 0.5, mScreenWidth / mScreenHeight, 0.01, 5000.0)
+        mShader.SetMatrixUniform("uViewProj", mView * mProjection)
+
+        'すべてのスプライトコンポーネントを描画
+        For Each sprite In mSprites
+            If sprite.GetVisible() = True Then
+                sprite.Draw(mShader)
+            End If
+        Next
+        mGame.SwapBuffers()
+    End Sub
+
+    Public Sub AddSprite(ByRef sprite As SpriteComponent)
+        Dim myDrawOrder As Integer = sprite.GetDrawOrder()
+        Dim cnt As Integer = mSprites.Count     '配列の要素数
+        Dim iter As Integer
+        If cnt > 0 Then
+            For iter = 0 To mSprites.Count - 1
+                If myDrawOrder < mSprites(iter).GetDrawOrder() Then
+                    Exit For
+                End If
+            Next
+        End If
+        mSprites.Insert(iter, sprite)
+    End Sub
+    Public Sub RemoveSprite(ByRef sprite As SpriteComponent)
+        Dim iter As Integer = mSprites.IndexOf(sprite)
+        '見つからなかったら-1が返される。
+        If iter >= 0 Then
+            mSprites.RemoveAt(iter)
+        End If
+    End Sub
+
+    Public Function GetTexture(ByRef filename As String) As Texture
+        Dim tex As Texture = Nothing
+        '画像ファイルが連想配列になければ追加する。
+        Dim b As Boolean = mTextures.ContainsKey(filename)
+        If b = True Then
+            'すでに読み込み済みなので、そのデータを返す。
+            tex = mTextures(filename)
+        Else
+            'Textureクラスを生成し、連想配列に追加する。
+            tex = New Texture()
+            If (tex.Load(filename)) Then
+                mTextures.Add(filename, tex)
+            Else
+                tex.Dispose()
+                tex = Nothing
+            End If
+        End If
+        Return tex
+    End Function
+
+    Public Function GetScreenWidth() As Double
+        Return mScreenWidth
+    End Function
+    Public Function GetScreenHeight() As Double
+        Return mScreenHeight
+    End Function
+
+    Public Sub SetViewMatrix(ByRef view As Matrix4)
+        mView = view
+    End Sub
+
+
+    'private
+    Private Sub CreateVertexInfo()
+        Dim numVerts As Integer = 16
+        '頂点座標(vector3)
+        Dim vertPos As Single() = {
+             -0.5, -0.5, 0.5,
+              0.5, -0.5, 0.5,
+             -0.5, -0.5, -0.5,
+              0.5, -0.5, -0.5,
+              0.5, -0.5, 0.5,
+              0.5, 0.5, 0.5,
+              0.5, -0.5, -0.5,
+              0.5, 0.5, -0.5,
+             -0.5, 0.5, 0.5,
+             -0.5, -0.5, 0.5,
+             -0.5, 0.5, -0.5,
+             -0.5, -0.5, -0.5,
+              0.5, 0.5, 0.5,
+             -0.5, 0.5, 0.5,
+              0.5, 0.5, -0.5,
+             -0.5, 0.5, -0.5
+        }
+        'テクスチャ座標(vector2)
+        Dim texCoord As Single() = {
+              0.0, 0.0,
+              1.0, 0.0,
+              0.0, 1.0,
+              1.0, 1.0,
+              0.0, 0.0,
+              1.0, 0.0,
+              0.0, 1.0,
+              1.0, 1.0,
+              0.0, 0.0,
+              1.0, 0.0,
+              0.0, 1.0,
+              1.0, 1.0,
+              0.0, 0.0,
+              1.0, 0.0,
+              0.0, 1.0,
+              1.0, 1.0
+        }
+        '頂点カラー(vector4 RGBA)
+        Dim vertColor As Single() = {
+              1.0, 0.0, 0.0, 1.0,
+              0.0, 1.0, 0.0, 1.0,
+              0.0, 0.0, 1.0, 1.0,
+              1.0, 1.0, 1.0, 1.0,
+              1.0, 0.0, 0.0, 1.0,
+              0.0, 1.0, 0.0, 1.0,
+              0.0, 0.0, 1.0, 1.0,
+              1.0, 1.0, 1.0, 1.0,
+              1.0, 0.0, 0.0, 1.0,
+              0.0, 1.0, 0.0, 1.0,
+              0.0, 0.0, 1.0, 1.0,
+              1.0, 1.0, 1.0, 1.0,
+              1.0, 0.0, 0.0, 1.0,
+              0.0, 1.0, 0.0, 1.0,
+              0.0, 0.0, 1.0, 1.0,
+              1.0, 1.0, 1.0, 1.0
+        }
+        'インデックス
+        Dim indices As UInteger() = {
+              0, 1, 2,
+              2, 1, 3,
+              4, 5, 6,
+              6, 5, 7,
+              8, 9, 10,
+              10, 9, 11,
+              12, 13, 14,
+              14, 13, 15
+        }
+        mVertexInfo = New VertexInfo(numVerts, vertPos, texCoord, vertColor, indices)
+    End Sub
+    Private Function LoadShaders() As Boolean
+        ' シェーダーを生成
+        mShader = New Shader()
+        If (mShader.Load("Shaders/shader.vert", "Shaders/shader.frag") <> True) Then
+            Return False
+        End If
+        Return True
+    End Function
+    Private disposedValue As Boolean
+    Private mGame As Game     'GameクラスはOpenTK.GameWindowの子クラス
+    Private mScreenWidth As Integer
+    Private mScreenHeight As Integer
+    Private mTextures As New Dictionary(Of String, Texture)
+    Private mSprites As New List(Of SpriteComponent)
+    Private mVertexInfo As VertexInfo
+    Private mShader As Shader
+    Private mView As Matrix4
+    Private mProjection As Matrix4
+End Class
---
+++ StudyGamePrograming04&05_VB/VertexInfo.vb
@@ -0,0 +1,110 @@
+﻿Imports System.Runtime.InteropServices
+Imports OpenTK
+Imports OpenTK.Graphics
+Imports OpenTK.Graphics.OpenGL
+
+Public Class VertexInfo
+    Implements IDisposable      '明示的にクラスを開放するために必要
+
+    'public
+    Public Sub New(ByVal numVerts As Integer, ByRef vertPos As Single(),
+                   ByRef texCoord As Single(), ByRef vertColor As Single(),
+                   ByRef indices As UInteger())
+        mNumVerts = numVerts
+
+        mVertexArray = GL.GenVertexArray()
+        GL.BindVertexArray(mVertexArray)
+
+        mIndexBuffer = GL.GenBuffer()
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, mIndexBuffer)
+        GL.BufferData(BufferTarget.ElementArrayBuffer,
+                      indices.Length * Marshal.SizeOf(Of UInteger),
+                      indices,
+                      BufferUsageHint.StaticDraw)
+
+        'VertexAttribute layout0 = position
+        mVertPosBuffer = GL.GenBuffer()
+        GL.BindBuffer(BufferTarget.ArrayBuffer, mVertPosBuffer)
+        GL.BufferData(BufferTarget.ArrayBuffer,
+                      vertPos.Length * Marshal.SizeOf(Of Single),
+                      vertPos,
+                      BufferUsageHint.StaticDraw)
+        GL.VertexAttribPointer(0,
+                               2,
+                               VertexAttribPointerType.Float,
+                               False,
+                               CInt(vertPos.Length / numVerts) * Marshal.SizeOf(Of Single),
+                               0)
+        GL.EnableVertexAttribArray(0)
+
+        'VertexAttribute layout1 = texCoord
+        mTexCoordBuffer = GL.GenBuffer()
+        GL.BindBuffer(BufferTarget.ArrayBuffer, mTexCoordBuffer)
+        GL.BufferData(BufferTarget.ArrayBuffer,
+                      texCoord.Length * Marshal.SizeOf(Of Single),
+                      texCoord,
+                      BufferUsageHint.StaticDraw)
+        GL.VertexAttribPointer(1,
+                               2,
+                               VertexAttribPointerType.Float,
+                               False,
+                               CInt(texCoord.Length / numVerts) * Marshal.SizeOf(Of Single),
+                               0)
+        GL.EnableVertexAttribArray(1)
+
+        'VertexAttribute layout2 = vertColor
+        mVertColorBuffer = GL.GenBuffer()
+        GL.BindBuffer(BufferTarget.ArrayBuffer, mVertColorBuffer)
+        GL.BufferData(BufferTarget.ArrayBuffer,
+                      vertColor.Length * Marshal.SizeOf(Of Single),
+                      vertColor,
+                      BufferUsageHint.StaticDraw)
+        GL.VertexAttribPointer(2,
+                               4,
+                               VertexAttribPointerType.Float,
+                               False,
+                               CInt(vertColor.Length / numVerts) * Marshal.SizeOf(Of Single),
+                               0)
+        GL.EnableVertexAttribArray(2)
+
+    End Sub
+    Protected disposed = False     '開放処理が実施済みかのフラグ
+    Public Overloads Sub Dispose() Implements IDisposable.Dispose
+        Dispose(True)
+    End Sub
+    Protected Overridable Overloads Sub Dispose(ByVal disposing As Boolean)
+        If Not Me.disposed Then
+            If disposing Then
+                '*** アンマネージリソースの開放
+                GL.DeleteBuffers(1, mVertPosBuffer)
+                GL.DeleteBuffers(1, mTexCoordBuffer)
+                GL.DeleteBuffers(1, mVertColorBuffer)
+                GL.DeleteBuffers(1, mIndexBuffer)
+                GL.DeleteVertexArrays(1, mVertexArray)
+            End If
+            '*** マネージドリソースの開放
+        End If
+        disposed = True
+    End Sub
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+        Dispose(False)
+    End Sub
+
+    Public Sub SetActive()
+        GL.BindVertexArray(mVertexArray)
+    End Sub
+    Public Function GetNumVerts() As Integer
+        Return mNumVerts
+    End Function
+
+    'private
+    Private disposedValue As Boolean
+    Private mNumVerts As Integer         ' 頂点の数
+    Private mVertexArray As Integer         ' バーテックス配列オブジェクトのOpenGL ID
+    Private mIndexBuffer As Integer         ' インデックスバッファのOpenGL ID
+    Private mVertPosBuffer As Integer        ' 頂点座標バッファのOpenGL ID
+    Private mTexCoordBuffer As Integer        ' テクスチャ座標バッファのOpenGL ID
+    Private mVertColorBuffer As Integer        ' 頂点カラーバッファのOpenGL ID
+
+End Class
